98ca8556bc5ca86fcf39231c8264454390bacdb3
shaburov <o.shaburov@tinkoff.ru>
2022-08-21 16:18:44 +0300

    Add KakafkaProducer

---
+++ client/src/main/java/kakafka/util/Serializer4Number.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright 2022 Shaburov Oleg
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package kakafka.util;
+
+import org.apache.kafka.common.serialization.Serializer;
+
+public class Serializer4Number<N extends Number> implements Serializer<N> {
+
+    @Override
+    public byte[] serialize(String topic, N data) {
+        return String.valueOf(data).getBytes();
+    }
+
+}
